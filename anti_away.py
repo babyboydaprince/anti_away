38ffccf5beaf0c965642ec3c51976c79427ce2a3
babyboydaprince <bala_zxz@hotmail.com>
2023-09-11 16:44:31 -0300

    it must be some kind of wizzardry...

--- anti_away.py
+++ anti_away.py
@@ -10,7 +10,7 @@ while i >= 1:
     keyboard.send("F13")
     time.sleep(5)
     print(f"\n{boo}")
-    print(f"A ghost is pressing F13...\n"
-          f"Does this key even exist?\n\n"
-          f"A ghost pressing a ghost key...\nWTF man...\n"
+    print("It must be some kind of wizardry\n"
+          "or a ghost...\n"
+          "WTF...\n"
           f"\nHit [ CTRL + C ] to call John Constantine {constantine}")
