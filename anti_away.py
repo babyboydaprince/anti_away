1fc813d5dd22d42ff13447f40cbb19776a794f58
babyboydaprince <bala_zxz@hotmail.com>
2023-09-14 20:47:21 -0300

    (add) Automated installation for Windows and Linux systems

--- anti_away.py
+++ anti_away.py
@@ -1,9 +1,11 @@
 import errno
+import os
 import emoji
 import sys
 import keyboard
 import time
 import platform
+import subprocess
 from pyfiglet import Figlet
 from termcolor import colored
 from colorama import Fore
@@ -29,6 +31,7 @@ class AntiAway:
 
         self.os_name = platform.system()
 
+        self.install_requirements()
         self.main()
 
 
@@ -71,7 +74,7 @@ class AntiAway:
 
     def always_here(self, value):
         if self.os_name == 'Linux':
-            # Maintaining F13 untill i find a ghost key
+            # Maintaining F13 until I find a ghost key
             key = 'F13'
         else:
             key = 'F13'
@@ -91,7 +94,8 @@ class AntiAway:
         while True:
             try:
                 interval = input("\nChoose the execution interval "
-                                 "time in seconds or milliseconds (numbers only).\n"
+                                 "time in seconds or "
+                                 "milliseconds (numbers only).\n"
                                  "Example: seconds -> 10, 120 "
                                  "and milliseconds -> 0.15, 0.5.\n\n"
                                  "----> ")
@@ -110,6 +114,81 @@ class AntiAway:
                 print(rf"{Fore.GREEN}Successful exorcism!{Fore.RESET}")
                 print('\n')
                 sys.exit(1)
+
+    def install_requirements(self):
+        req_file_w = '.\\requirements.txt'
+        req_file_l = './requirements.txt'
+
+        try:
+            if self.os_name == 'Windows':
+
+                with open(req_file_w, 'r') as file:
+                    requirements = file.read().splitlines()
+
+                user_input = input("\nHint: If requirements were "
+                                   "already installed, you can "
+                                   "choose ( n ) to proceed.\n"
+                                   "Do you want to proceed with "
+                                   "the installation? (y/n): ")
+
+                if user_input.lower() == 'y':
+                    subprocess.check_call(['pip', 'install'] + requirements)
+                    time.sleep(2)
+                    os.system('cls')
+                else:
+                    print("\nInstallation aborted.")
+                    time.sleep(2)
+                    os.system('cls')
+
+            elif self.os_name == 'Linux':
+                with open(req_file_l, 'r') as file:
+                    requirements = file.read().splitlines()
+
+                if os.geteuid() == 0:
+                    # Running as root
+                    user_input = input("\nHint: If requirements were "
+                                       "already installed, you can "
+                                       "choose ( n ) to proceed.\n"
+                                       "Do you want to proceed with "
+                                       "the installation? (y/n): ")
+
+                    if user_input.lower() == 'y':
+                        subprocess.check_call(['pip', 'install'] +
+                                              requirements)
+                        time.sleep(2)
+                        os.system('clear')
+
+                    else:
+                        print("\nInstallation aborted.")
+                        time.sleep(2)
+                        os.system('clear')
+                else:
+                    print("It's recommended to run this script as root (sudo) "
+                          "on Linux for system-wide installation.\n")
+
+                    print("You can run the script as root using: "
+                          "sudo python anti_away.py\n")
+
+                    print("If you want to install packages in a "
+                          "virtual environment, use a virtual environment.\n")
+
+                    user_input = input("\nDo you want to proceed "
+                                       "with the installation? (y/n): ")
+
+                    if user_input.lower() == 'y':
+                        subprocess.check_call(['pip', 'install'] +
+                                              requirements)
+                        time.sleep(2)
+                        os.system('clear')
+                    else:
+                        print("\nInstallation aborted.")
+                        time.sleep(2)
+                        os.system('clear')
+            else:
+                print("\nUnsupported operating system:", self.os_name)
+
+        except FileNotFoundError:
+            print("\nrequirements.txt file not found.")
 
 
 if __name__ == '__main__':
